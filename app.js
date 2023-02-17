c0408d7a3c534ecde4443bf8293d7724d65f6d8c
Rioba-Ian <riobafelix@gmail.com>
2023-02-17 06:59:14 +0300

    clear code and start again

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-let displayVal
 
-let operandSelected = false;
 
 const operate = (operator) =>{
     return operator
 }
 
-const calcObjects = {
-    nums: [],
-    operator: [],
-}
+
 // on the display screen display contents of the 
 const outputDisplay = document.querySelector("#output")
 outputDisplay.textContent = displayVal
@@ -20,78 +15,35 @@ const pointCalc = document.querySelector("button[name='point']")
 
 
 // event listeners
-buttonsCalc.forEach((item)=> {
-    item.addEventListener('click', getBtnValue)
-})
-
-operandCalc.forEach((item)=> {
-    item.addEventListener('click', getOperValue)
-})
-
-pointCalc.addEventListener("click", ()=> {
-    if (!operandSelected){
-        handlePoint 
-    }
-}, {once: true})
-
-if(operandSelected){
-    pointCalc.removeEventListener("click", handlePoint)
-}
-
-
-// handlers for event listeners
-function getBtnValue(e){
-    
-    calcObjects["nums"].push(e.target.value)
-    displayVal = calcObjects.nums[0]
-
-    outputDisplay.textContent += calcObjects["nums"].at(-1)
-}
-
-function getOperValue(e){
-    calcObjects["operator"].push(e.target.id)
-    calcObjects["nums"].push(",")
-
-    operandSelected = false 
-
-    outputDisplay.textContent += e.target.value
-}
-
-function handlePoint(){
-    outputDisplay.textContent += "."
-    calcObjects["nums"].splice(-1, 1, calcObjects.nums.slice(-1)+".")  
-}
-
-
-
-// let test_arr = []
-// let test1 = ['2','3',',','4', ","]
-// test_arr = test1.join("")
-//                 .split(",")
-//                 .map(item => Number(item))
-// result = add(test_arr)
-// console.log(add(test_arr))
-
-// const posOperators = test1.reduce((acc, val, iter)=> 
-//                     (val === "," && acc.push(iter), acc),    
-//                     [])
 
 
 
 
 
-// function createResult(obj){
-//     if (obj.nums.length == 4){
-//         obj["pairNums"] = obj["nums"].join("")
-//                             .split(",")
-//                             .map(item => Number(item))
-//     }
 
-    
-//     return obj
-// }
 
-// createResult(calcObjects)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
